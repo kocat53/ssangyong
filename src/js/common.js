cf50e77429ab2ae0803db5ebc59ff6e5189283e5
박종상 <nt99217@nhn.com>
2019-08-20 00:55:05 +0900

    헤더 어느정도 완료

--- src/js/common.js
+++ src/js/common.js
@@ -10,24 +10,38 @@ function headerSearch() {
 
 function gnb() {
 	$('#gnb > li').each(function () {
-		var $this = $(this);
+		var $this = $(this),
+			$depth2 = $this.find('.gnb_depth1> li');
 
+		// 포커스 in
 		$this.on('mouseenter focusin', function () {
 			$this.addClass('on').siblings().removeClass('on');
 			
-			var $maxHeight = Math.max.apply(null, $this.find('.gnb_depth1 > li').map(function (){
-				return $(this).height();
+			// 높이 구하기
+			var $maxHeight = Math.max.apply(null, $this.find('li').map(function (){
+				return $(this).innerHeight();
 			}).get());
-			
-			console.log($maxHeight);
-			$('.gnb_bg').addClass('on').css({
+
+			// 넓이 구하기
+			var $width = (100 / $depth2.length).toFixed(3);
+						
+			$depth2.css({
+				'width': $width+"%",
+				'height': $maxHeight
+			});
+
+			$('.gnb_bg').css({
 				'display':'block',
 				'height': $maxHeight
 			});
+
+			$('.gnb_dimmed').addClass('on');
 		});
 
+		// 마우스 out
 		$this.on('mouseleave', function () {
-			$this.removeClass('on');			
+			$this.removeClass('on');		
+			$('.gnb_dimmed').removeClass('on');
 			$('.gnb_bg').removeAttr('style');
 		});
 	});
